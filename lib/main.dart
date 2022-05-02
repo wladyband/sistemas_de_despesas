776c4cc7b19060139856eee024e639701829e89d
Wladimir Bandeira <tecnico100@gmail.com>
2022-05-02 15:35:55 -0300

    realizando primeira refatoração - aula 103

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:despesas/components/transaction_form.dart';
+import 'package:despesas/components/transaction_list.dart';
 import 'package:despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -17,8 +19,7 @@ class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
 
 
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
+
 
   final _transactions = [
     Transaction(
@@ -54,87 +55,8 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple
-                        ),
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                            tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(tr.date),
-                          style: TextStyle(
-                            color: Colors.grey
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                TextField(
-                  controller: titleController,
-                  decoration: InputDecoration(
-                    labelText: 'Título'
-                  ),
-                ),
-                TextField(
-                  controller: valueController,
-                  decoration: InputDecoration(
-                      labelText: 'Valor (R\$)'
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FlatButton(
-                      child: Text('Nova Transação'),
-                      textColor: Colors.purple,
-                      onPressed: () {
-                        print(titleController.text);
-                        print(valueController.text);
-                      },
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
+          TransactionList(_transactions),
+          TransactionForm(),
         ],
       )
     );
